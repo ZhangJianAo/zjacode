0d5dbd51484043d321176eaccac59cde8c1fac62
zhangjianao <zhangjianao@139f5c23-213c-0410-9c76-5f19dca96c1e>
2007-10-16 17:22:52 +0000

    on going

--- iphone/iphonebook/iPhoneBookApplication.m
+++ iphone/iphonebook/iPhoneBookApplication.m
@@ -15,6 +15,66 @@
 
 #define NAV_BAR_HEIGHT 45.0f
 
+- (void) AddRow: (NSString*)title
+{
+	UIImageAndTextTableCell *cell = [[UIImageAndTextTableCell alloc] init];
+	[cell setTitle: title];
+	[_pbCells addObject: cell];
+	[cell release];
+}
+
+char GetChar(char *p)
+{
+	switch(*p) {
+	case '0': return 0;
+	case '1': return 1;
+	case '2': return 2;
+	case '3': return 3;
+	case '4': return 4;
+	case '5': return 5;
+	case '6': return 6;
+	case '7': return 7;
+	case '8': return 8;
+	case '9': return 9;
+	case 'A': return 10;
+	case 'B': return 11;
+	case 'C': return 12;
+	case 'D': return 13;
+	case 'E': return 14;
+	case 'F': return 15;
+	}
+
+	return 0;
+}
+
+char GetHexChar(char *p)
+{
+	char h = GetChar(p);
+	char l = GetChar(p+1);
+
+	return (h<<4 | l);
+}
+
+- (NSString *) Ucs2String: (char *)str
+{
+	char buf[255];
+	char *p = str;
+	int len = 0;
+
+	buf[0] = 0xFE; buf[1] = 0xFF;
+	len = 2;
+
+	while('\0' != *p) {
+		buf[len] = GetHexChar(p);
+		p += 2;
+		len += 1;
+	}
+
+	NSData *dat = [NSData dataWithBytesNoCopy: buf length:len];
+	
+	return [[NSString alloc] initWithData: dat encoding:NSUnicodeStringEncoding];
+}
+
 - (void) applicationDidFinishLaunching: (id)unknown
 {
 	struct CGRect rect = [UIHardware fullScreenApplicationContentRect];
@@ -54,13 +114,16 @@
 	[_pbCells addObject: cell];
 	[cell release];
 
-	rect = [UIHardware fullScreenApplicationContentRect];
+	//[self AddRow: [self Ucs2String: "707573AF4E3D0041004300430054"]];
 
-	cell = [[UIImageAndTextTableCell alloc] init];
-	[cell setTitle: [NSString stringWithFormat: @"ReadAllPB: %d", ReadAllPB()->count]];
-	[_pbCells addObject: cell];
-	[cell release];
+	sim_phonebook *spb = ReadAllPB();
 
+	[self AddRow: [NSString stringWithFormat: @"count: %d", spb->count]];
+
+	int i = 0;
+	for(i = 0; i < spb->count; i++) {
+		[self AddRow: [NSString stringWithFormat: @"%s: %@", spb->numbers[i], [self Ucs2String: spb->names[i]]]];
+	}
 	[pbTable reloadData];
 }
 
