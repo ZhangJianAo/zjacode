9273b3637b34e3c7740294663cc9f5d73fa25750
zhangjianao <zhangjianao@139f5c23-213c-0410-9c76-5f19dca96c1e>
2007-10-29 17:03:34 +0000

    long time commit

--- iphone/iphonebook/iPhoneBookApplication.h
+++ iphone/iphonebook/iPhoneBookApplication.h
@@ -4,9 +4,13 @@
 
 @interface iPhoneBookApplication : UIApplication {
 	UIWindow *_window;
+	UIView *_mainView;
+	UITable *_pbTable;
 	NSMutableArray *_pbCells;
+	UIAlertSheet *errorInfoSheet;
 }
 
 - (void) AddRow: (NSString *)title;
+- (void)alertSheet:(UIAlertSheet *)sheet buttonClicked:(int)button;
 
 @end
--- iphone/iphonebook/sim_phonebook.c
+++ iphone/iphonebook/sim_phonebook.c
@@ -26,6 +26,13 @@ static struct termios gOriginalTTYAttrs;
 #define DEBUGLOG(x) x
 #endif
 
+static char lastError[1024];
+
+char * sim_get_lasterror()
+{
+	return lastError;
+}
+
 #define UINT(x) *((unsigned int *) (x))
 
 void SendCmd(int fd, void *buf, size_t size)
@@ -78,6 +85,169 @@ int GetAtReturn(int fd)
 	}
 
 	return -1;
+}
+
+// Given the path to a serial device, open the device and configure it.
+// Return the file descriptor associated with the device.
+static int OpenSerialPort(const char *bsdPath)
+{
+	int fileDescriptor = -1;
+	int handshake;
+	struct termios  options;
+    
+	// Open the serial port read/write, with no controlling terminal, and don't wait for a connection.
+	// The O_NONBLOCK flag also causes subsequent I/O on the device to be non-blocking.
+	// See open(2) ("man 2 open") for details.
+	fileDescriptor = open(bsdPath, O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if (fileDescriptor == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error opening serial port %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+		goto error;
+	}
+
+	// Note that open() follows POSIX semantics: multiple open() calls to the same file will succeed
+	// unless the TIOCEXCL ioctl is issued. This will prevent additional opens except by root-owned
+	// processes.
+	// See tty(4) ("man 4 tty") and ioctl(2) ("man 2 ioctl") for details.
+	if (ioctl(fileDescriptor, TIOCEXCL) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error setting TIOCEXCL on %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+		goto error;
+	}
+    
+	// Now that the device is open, clear the O_NONBLOCK flag so subsequent I/O will block.
+	// See fcntl(2) ("man 2 fcntl") for details.
+	if (fcntl(fileDescriptor, F_SETFL, 0) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error clearing O_NONBLOCK %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+		goto error;
+	}
+    
+	// Get the current options and save them so we can restore the default settings later.
+	if (tcgetattr(fileDescriptor, &gOriginalTTYAttrs) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error getting tty attributes %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+		goto error;
+	}
+
+	// The serial port attributes such as timeouts and baud rate are set by modifying the termios
+	// structure and then calling tcsetattr() to cause the changes to take effect. Note that the
+	// changes will not become effective without the tcsetattr() call.
+	// See tcsetattr(4) ("man 4 tcsetattr") for details.
+	options = gOriginalTTYAttrs;
+    
+	// Print the current input and output baud rates.
+	// See tcsetattr(4) ("man 4 tcsetattr") for details.
+	/*
+	printf("Current input baud rate is %d\n", (int) cfgetispeed(&options));
+	printf("Current output baud rate is %d\n", (int) cfgetospeed(&options));
+	*/
+    
+	// Set raw input (non-canonical) mode, with reads blocking until either a single character 
+	// has been received or a one second timeout expires.
+	// See tcsetattr(4) ("man 4 tcsetattr") and termios(4) ("man 4 termios") for details.
+	cfmakeraw(&options);
+	options.c_cc[VMIN] = 1;
+	options.c_cc[VTIME] = 10;
+        
+	// The baud rate, word length, and handshake options can be set as follows:
+	cfsetspeed(&options, B115200);    // Set 19200 baud    
+	/*
+	options.c_cflag |= (CS7      |   // Use 7 bit words
+			    PARENB     |   // Parity enable (even parity if PARODD not also set)
+			    CCTS_OFLOW |   // CTS flow control of output
+			    CRTS_IFLOW);  // RTS flow control of input
+	*/ 
+	//options.c_cflag = CS7 | CLOCAL | CREAD; 
+	// Print the new input and output baud rates. Note that the IOSSIOSPEED ioctl interacts with the serial driver 
+	// directly bypassing the termios struct. This means that the following two calls will not be able to read
+	// the current baud rate if the IOSSIOSPEED ioctl was used but will instead return the speed set by the last call
+	// to cfsetspeed.
+	/*
+	printf("Input baud rate changed to %d\n", (int) cfgetispeed(&options));
+	printf("Output baud rate changed to %d\n", (int) cfgetospeed(&options));
+	*/
+    
+	// Cause the new options to take effect immediately.
+	if (tcsetattr(fileDescriptor, TCSANOW, &options) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error setting tty attributes %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+		goto error;
+	}
+
+	// To set the modem handshake lines, use the following ioctls.
+	// See tty(4) ("man 4 tty") and ioctl(2) ("man 2 ioctl") for details.
+	if (ioctl(fileDescriptor, TIOCSDTR) == -1) // Assert Data Terminal Ready (DTR)
+	{
+		snprintf(lastError, sizeof(lastError), "Error asserting DTR %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+	}
+    
+	if (ioctl(fileDescriptor, TIOCCDTR) == -1) // Clear Data Terminal Ready (DTR)
+	{
+		snprintf(lastError, sizeof(lastError), "Error clearing DTR %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+	}
+    
+	handshake = TIOCM_DTR | TIOCM_RTS | TIOCM_CTS | TIOCM_DSR;
+	if (ioctl(fileDescriptor, TIOCMSET, &handshake) == -1)
+		// Set the modem lines depending on the bits set in handshake
+	{
+		snprintf(lastError, sizeof(lastError), "Error setting handshake lines %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+	}
+    
+	// To read the state of the modem lines, use the following ioctl.
+	// See tty(4) ("man 4 tty") and ioctl(2) ("man 2 ioctl") for details.
+    
+	if (ioctl(fileDescriptor, TIOCMGET, &handshake) == -1)
+		// Store the state of the modem lines in handshake
+	{
+		snprintf(lastError, sizeof(lastError), "Error getting handshake lines %s - %s(%d).\n",
+		       bsdPath, strerror(errno), errno);
+	}
+    
+	//printf("Handshake lines currently set to %d\n", handshake);
+  
+	// Success
+	return fileDescriptor;
+    
+	// Failure path
+error:
+	if (fileDescriptor != -1)
+	{
+		close(fileDescriptor);
+	}
+    
+	return -1;
+}
+// Given the file descriptor for a serial device, close that device.
+void CloseSerialPort(int fileDescriptor)
+{
+	// Block until all written output has been sent from the device.
+	// Note that this call is simply passed on to the serial device driver. 
+	// See tcsendbreak(3) ("man 3 tcsendbreak") for details.
+	if (tcdrain(fileDescriptor) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error waiting for drain - %s(%d).\n",
+		       strerror(errno), errno);
+	}
+    
+	// Traditionally it is good practice to reset a serial port back to
+	// the state in which you found it. This is why the original termios struct
+	// was saved.
+	if (tcsetattr(fileDescriptor, TCSANOW, &gOriginalTTYAttrs) == -1)
+	{
+		snprintf(lastError, sizeof(lastError), "Error resetting tty attributes - %s(%d).\n",
+		       strerror(errno), errno);
+	}
+
+	close(fileDescriptor);
 }
 
 int InitConn()
@@ -140,7 +310,7 @@ typedef unsigned char	UCHAR;
 typedef unsigned char	*LPSTR;
 typedef unsigned char	*LPBYTE;
 
-sim_phonebook * ReadAllPB()
+sim_phonebook * sim_read_pb()
 {
 	char buf[1024];
 	int buf_len, process_len, i;
@@ -149,7 +319,12 @@ sim_phonebook * ReadAllPB()
 	int begin, end;
 	int len;
 
-	int fd = InitConn(); 
+	//int fd = InitConn(); 
+	int fd = OpenSerialPort("/dev/tty.debug");
+	if (fd < 0) {
+		return NULL;
+	}
+
 	AT(fd);  
 	SendStrCmd(fd, "ATE0\r");
 	GetAtReturn(fd);
@@ -178,7 +353,8 @@ sim_phonebook * ReadAllPB()
 		ParsePhoneBookLine(buf, buf_len, result);
 	}
 
-	CloseConn(fd);
+	//CloseConn(fd);
+	CloseSerialPort(fd);
 
 	return result;
 }
